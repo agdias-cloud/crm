049b0f4207b9e4a11a469c5aa1747090b9eb187b
unknown <agdias@stefanini.com>
2024-07-18 08:31:54 -0300

    .

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # crm
+
+
+
+wget -O /tmp/helm-v3.15.3-linux-amd64.tar.gz  https://get.helm.sh/helm-v3.15.3-linux-amd64.tar.gz
--- app/server.js
+++ app/server.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const connectDB = require('./config/db')
-//const dotenv = require('dotenv').config();
+const dotenv = require('dotenv').config();
 const pacientes = require('./src/routes/pacientes')
 const port=process.env.PORT
-console.log(port)
-//connectDB();
+
+connectDB();
 const app = express();
 
 app.use(express.json());
 app.use('/api/v1/pacientes', pacientes);
 
-//app.listen(port, () => console.log(`Running on port ${port}`))
+app.listen(port, () => console.log(`Running on port ${port}`))
 
